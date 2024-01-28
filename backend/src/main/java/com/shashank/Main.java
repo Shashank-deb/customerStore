cbc9b94a0c85e5d23478615f5bd242a7b1558705
Shashank Sharma <sharmashashank446@gmail.com>
2024-01-28 15:54:26 +0530

    Removing the unused line and imports in Main.java

--- backend/src/main/java/com/shashank/Main.java
+++ backend/src/main/java/com/shashank/Main.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Random;
-import java.util.UUID;
+
 
 
 @SpringBootApplication
